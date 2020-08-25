127e11bec4bf5c42e8fe65e386584aedc4cba9c2
keshav <skeshav162@gmail.com>
2020-08-25 18:41:48 +0530

    HubSpot-Clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import HeaderTwo from './HeaderTwo';
+import CardOne from './CardOne';
+import CardTwo from './CardTwo';
+import CardThree from './CardThree';
+import CardFour from './CardFour';
+import Footer from './Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <HeaderTwo />
+      <CardOne />
+      <CardTwo />
+      <CardThree />
+      <CardFour />
+      <Footer />
     </div>
   );
 }
